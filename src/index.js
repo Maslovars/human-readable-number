a94fc0291945a4c0604a16372559189fcc6756f0
Maslovars <maslovars@gmail.com>
2021-08-16 21:31:09 +0300

    fix: fix warnings

--- src/index.js
+++ src/index.js
@@ -27,10 +27,9 @@ module.exports = function toReadable(number) {
         'eighty',
         'ninety'];
 
-    if (number == undefined || number == 0) return 'zero';
+    if (number === undefined || number === 0) return 'zero';
     if (number <= 9) return firstNum[number - 1];
     if (number <= 19) return secondNum[number - 10];
-    if (number <= 99) return `${thirdNum[Math.floor(number / 10) - 2]}${number % 10 != 0 ? ' ' + firstNum[number % 10 - 1] : ''}`;
-
-    return `${firstNum[Math.floor(number / 100) - 1]} hundred${number % 100 != 0 ? ' ' + toReadable(number % 100) : ''}`;
+    if (number <= 99) return `${thirdNum[Math.floor(number / 10) - 2]}${number % 10 !== 0 ? ' ' + firstNum[number % 10 - 1] : ''}`;
+    return `${firstNum[Math.floor(number / 100) - 1]} hundred${number % 100 !== 0 ? ' ' + toReadable(number % 100) : ''}`;
 }
